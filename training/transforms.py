5c2ba79e64154e3b4d88f6b2ee89db787a3674de
Thomas Dagonneau <thdaga@ge.polymtl.ca@tassan.neuro.polymtl.ca>
2025-11-26 15:52:59 -0500

    label interpolation is now done using nn

--- training/transforms.py
+++ training/transforms.py
@@ -21,7 +21,7 @@ def get_transforms(img_size,resolution,augment= True,prob_flip= 0.2,prob_affine=
     base = [LoadImaged(keys=keys_both, allow_missing_keys=True),
             EnsureChannelFirstd(keys=keys_both, allow_missing_keys=True),
             Orientationd(keys=keys_both, axcodes="RPI",labels=None, allow_missing_keys=True),
-            Spacingd(keys=keys_both,pixdim=resolution,mode=("bilinear"),allow_missing_keys=True),
+            Spacingd(keys=keys_both,pixdim=resolution,mode=("bilinear", "nearest"),allow_missing_keys=True),
             ResizeWithPadOrCropd(keys=keys_both,spatial_size=img_size,allow_missing_keys=True),
             ScaleIntensityd(keys=keys_img),
             EnsureTyped(keys=keys_both,dtype=torch.float32,track_meta=False,allow_missing_keys=True),
