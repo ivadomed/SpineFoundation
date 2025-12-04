e708fdeafecc2d8da8b5855f35795d640e53ab3a
Thomas Dagonneau <thdaga@ge.polymtl.ca@tassan.neuro.polymtl.ca>
2025-12-04 13:45:04 -0500

    removed comments in French, added data augmentation to the transforms when called

--- data_management/dataloader.py
+++ data_management/dataloader.py
@@ -13,21 +13,21 @@ from monai.data import Dataset as MonaiDataset
 import json
 
 import torch.multiprocessing as mp
-mp.set_sharing_strategy("file_system") #permets d'augmenter le nombre de workers
+mp.set_sharing_strategy("file_system") # To have more workers than default limit (which is 4 on many systems)
 
 
 from .json_data_creator import create_data_manifest
 from .transforms_cpu import get_transforms_cpu
 
-def makemonaidataset(data_list):
+def makemonaidataset(data_list, augment=False):
 
-    transforms = get_transforms_cpu()
+    transforms = get_transforms_cpu(augment=augment)
     return MonaiDataset(data=data_list, transform=transforms)
 
 
 def build_dataloaders(batch_size,splits=(0.8, 0.1, 0.1),num_workers=2,shuffle_seed=None,data_path=False,json_path=False,json_save=False):
     
-    try: #Si le JSON existe
+    try: 
         json_path = os.path.abspath(Path(json_path))
         with open(json_path, 'r') as f:
             data_manifest = json.load(f)
@@ -51,12 +51,11 @@ def build_dataloaders(batch_size,splits=(0.8, 0.1, 0.1),num_workers=2,shuffle_se
 
 
     print("\n")
-    # 2. Create Datasets using the pre-loaded data lists
-    # Training usually requires heavy augmentation (augment=True) for MAE
-    train_ds = makemonaidataset(train_data)
-    # Validation/Test typically use minimal or no augmentation (augment=False)
-    val_ds = makemonaidataset(val_data)
-    test_ds = makemonaidataset(test_data)
+
+    train_ds = makemonaidataset(train_data, augment=True)
+
+    val_ds = makemonaidataset(val_data, augment=False)
+    test_ds = makemonaidataset(test_data, augment=False)
 
     # 3. Create DataLoaders
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers,pin_memory=True,persistent_workers=False,prefetch_factor=1,collate_fn=list_collate)
