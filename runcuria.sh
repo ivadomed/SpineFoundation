71d4d7a048295cd779233ca5c85629c6db963201
Julien Laborde-Peyré <julien.lbrd@gmail.com>
2026-04-08 09:59:06 -0400

    Script pour entrainement curia

--- runcuria.sh
+++ runcuria.sh
@@ -2,4 +2,5 @@ cd /home/ge.polymtl.ca/p123239/SpineFoundation && \
 CUDA_VISIBLE_DEVICES=0,1 \
 /home/ge.polymtl.ca/p123239/.conda/envs/dino/bin/python -m torch.distributed.run \
   --nnodes 1 --nproc-per-node 2 --master_port 29501 \
-  train_dino.py --train_config_file /home/ge.polymtl.ca/p123239/SpineFoundation/configs/dino/configcuria.yaml
+  train_dino.py --train_config_file /home/ge.polymtl.ca/p123239/SpineFoundation/configs/dino/configcuria.yaml \
+  2>&1 | tee /home/ge.polymtl.ca/p123239/SpineFoundation/outputs_curia_ft/run_stderr.log
